02119b0536ced90c36f05b6b30cad6c34fe250a1
ashantys <100235782+ashantys@users.noreply.github.com>
2024-12-05 19:52:36 -0600

    Rutas

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { Route } from "react-router-dom"
+
+
+export const  AppRouter = () => {
+    return ( 
+        <Route>
+            {/* Login y Registro */}
+            <Route/>
+
+            {/* JournalApp */}
+            <Route/>
+
+
+        </Route>
+     )
+}
+
